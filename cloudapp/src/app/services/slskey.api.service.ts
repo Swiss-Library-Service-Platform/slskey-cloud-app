185e61f63c72df28b471dfe2f5964f67890f01ca
v1lling <sascha.villing@web.de>
2026-09-10 13:08:24 +0200

    Refactor testInstitutions initialization for clarity

--- cloudapp/src/app/services/slskey.api.service.ts
+++ cloudapp/src/app/services/slskey.api.service.ts
@@ -31,8 +31,8 @@ export class SlskeyAPIService {
   private baseUrlTest: string = 'https://slskey2-test.swisscovery.network';
   private cloudAppPath: string = '/api/v1/cloudapp';
 
-  private testInstitutions: Array<string> = ['41SLSP_UBS'];
-  //private testInstitutions: Array<string> = [];
+  //private testInstitutions: Array<string> = ['41SLSP_UBS'];
+  private testInstitutions: Array<string> = [];
   private isTestEnvironment: boolean = false;
   private readonly _isTestEnvironmentObject = new BehaviorSubject<boolean>(false);
 
